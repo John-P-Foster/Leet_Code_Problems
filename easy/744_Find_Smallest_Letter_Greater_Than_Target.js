aa5cf6a887706534e9551126141f3a9244503417
John-P-Foster <johnpete.foster@gmail.com>
2025-07-06 16:44:26 -0400

    Finished smalles letter

--- easy/744_Find_Smallest_Letter_Greater_Than_Target.js
+++ easy/744_Find_Smallest_Letter_Greater_Than_Target.js
@@ -1,6 +1,8 @@
-// You are given an array of characters letters that is sorted in non-decreasing order, and a character target. There are at least two different characters in letters.
+// You are given an array of characters letters that is sorted in non-decreasing order, 
+// and a character target. There are at least two different characters in letters.
 
-// Return the smallest character in letters that is lexicographically greater than target. If such a character does not exist, return the first character in letters.
+// Return the smallest character in letters that is lexicographically greater than target. 
+// If such a character does not exist, return the first character in letters.
 
  
 
@@ -28,3 +30,52 @@
 // letters is sorted in non-decreasing order.
 // letters contains at least two different characters.
 // target is a lowercase English letter.
+
+/**
+ * @param {character[]} letters
+ * @param {character} target
+ * @return {character}
+ */
+var nextGreatestLetterBrute = function(letters, target) {
+    for(let i = 0; i < letters.length; i ++){
+        if(letters[i] > target){
+            return letters[i]
+        }
+    }
+    return letters[0]; 
+};
+
+
+var nextGreatestLetterBinary = function(letters, target) {
+    let left = 0;
+    let right = letters.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if (letters[mid] <= target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return letters[left % letters.length];
+};
+
+
+
+let testCases = [
+    ["c","f","j"],
+    ["c","f","j"],
+    ["x","x","y","y"],
+]
+
+let targets = [
+    "a",
+    "c",
+    "z"
+]
+testCases.forEach((test, i) =>{
+console.log(nextGreatestLetterBinary(test, targets[i]))
+});
