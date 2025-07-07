55155e45d41d69304162587e14df1bfffa32d70a
John-P-Foster <johnpete.foster@gmail.com>
2025-07-07 14:33:31 -0400

    Finished min time problem.

--- easy/1266_Minimum_Time_Visiting_All_Points.js
+++ easy/1266_Minimum_Time_Visiting_All_Points.js
@@ -31,3 +31,36 @@
 // 1 <= n <= 100
 // points[i].length == 2
 // -1000 <= points[i][0], points[i][1] <= 1000
+
+
+/**
+ * @param {number[][]} points
+ * @return {number}
+ */
+var minTimeToVisitAllPoints = function(points) {
+    // compair [x, y] and [x2, y2] sub the difference and move set x, y to the new setting and log the step
+    // count. 
+    let seconds = 0; 
+
+    for(let i = 0; i < points.length -1; i ++){
+        let x1 = points[i][0];
+        let x2 = points[i + 1][0];
+        let y1 = points[i][1];
+        let y2 = points[i+ 1][1];
+        let xDif = Math.abs(x1 - x2); 
+        let yDif = Math.abs(y1 - y2); 
+        
+        seconds += Math.max(xDif, yDif)
+       
+   }
+   return seconds; 
+};
+
+const testCases = [
+    [[1,1],[3,4],[-1,0]],
+    [[3,2],[-2,2]]
+]
+
+for(let test of testCases){
+    console.log(minTimeToVisitAllPoints(test))
+}
