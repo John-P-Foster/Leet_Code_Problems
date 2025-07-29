bdb386cf5398cf32f08ca0b182e17c4ff620c11c
John-P-Foster <johnpete.foster@gmail.com>
2025-07-29 09:53:50 -0400

    Worked problem 1317 get no zero integer pairs that sum to n.

---
+++ easy/1317_Convert_Integer_to_the_Sum_of_Two_None_Zero_Intgers.js
@@ -0,0 +1,59 @@
+// No-Zero integer is a positive integer that does not contain any 0 in its decimal representation.
+
+// Given an integer n, return a list of two integers [a, b] where:
+
+// a and b are No-Zero integers.
+// a + b = n
+// The test cases are generated so that there is at least one valid solution. If there are many
+//  valid solutions, you can return any of them.
+
+ 
+
+// Example 1:
+
+// Input: n = 2
+// Output: [1,1]
+// Explanation: Let a = 1 and b = 1.
+// Both a and b are no-zero integers, and a + b = 2 = n.
+// Example 2:
+
+// Input: n = 11
+// Output: [2,9]
+// Explanation: Let a = 2 and b = 9.
+// Both a and b are no-zero integers, and a + b = 11 = n.
+// Note that there are other valid answers as [8, 3] that can be accepted.
+ 
+
+// Constraints:
+
+// 2 <= n <= 104
+
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+var getNoZeroIntegers = function(n) {
+
+    const hasZero = num => num.toString().includes('0'); 
+
+    for(let i = 1; i <= n-1; i ++){
+        let a = i; 
+        let b = n - i; 
+        if(hasZero(a) || hasZero(b)){
+            continue; 
+        }
+        return [a, b]; 
+    }
+
+};
+
+const testCases = [
+    [2, [1,1]],
+    [101, [2,99]]
+]
+
+for(let [input, expected] of testCases){
+    let returned = getNoZeroIntegers(input); 
+    let result = returned.toString() === expected.toString() ? `Passed with: ${returned.toString()}` : `Failed got: ${returned.toString()}`; 
+    console.log(result);
+}
