8be6ab31ee61cac23450f49970f6b83a4cc2e746
John-P-Foster <johnpete.foster@gmail.com>
2025-09-08 09:32:15 -0400

    worked minimum common value is sorted array.

---
+++ easy/2540_Minimum_Common_Value.js
@@ -0,0 +1,60 @@
+// Given two integer arrays nums1 and nums2, sorted in non-decreasing order, return the minimum integer 
+// common to both arrays. If there is no common integer amongst nums1 and nums2, return -1.
+
+// Note that an integer is said to be common to nums1 and nums2 if both arrays have at least one 
+// occurrence of that integer.
+
+ 
+
+// Example 1:
+
+// Input: nums1 = [1,2,3], nums2 = [2,4]
+// Output: 2
+// Explanation: The smallest element common to both arrays is 2, so we return 2.
+// Example 2:
+
+// Input: nums1 = [1,2,3,6], nums2 = [2,3,4,5]
+// Output: 2
+// Explanation: There are two common elements in the array 2 and 3 out of which 2 is the smallest, so
+//  2 is returned.
+ 
+
+// Constraints:
+
+// 1 <= nums1.length, nums2.length <= 105
+// 1 <= nums1[i], nums2[j] <= 109
+// Both nums1 and nums2 are sorted in non-decreasing order.
+
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var getCommon = function(nums1, nums2) {
+    let index1 = 0; 
+    let index2 = 0; 
+
+    while(index1 < nums1.length && index2 < nums2.length){
+        if(nums1[index1] === nums2[index2]) return nums1[index1]; 
+        if(nums1[index1] < nums2[index2]) index1 ++; 
+        if(nums1[index1] > nums2[index2]) index2 ++; 
+    }
+
+    return -1; 
+};
+
+const testCases = [
+    [[1,2,3], [2,4], 2],
+    [[1,2,3,6], [2,3,4,5], 2],
+    [[1], [1], 1],
+    [[1], [2], -1]
+]
+
+let testNumber = 1; 
+for(let  [nums1, nums2, expected] of testCases){
+    const returned = getCommon(nums1, nums2); 
+    const result = returned === expected ? `Test ${testNumber} ✅ Passed with: ${returned}` : `Test ${testNumber}❌ Failed with: ${returned}`;
+    testNumber ++; 
+    console.log(result); 
+}
