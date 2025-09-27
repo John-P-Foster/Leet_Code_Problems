b57b8efe23ae5436da18ed04871e48d54b1f57cf
John-P-Foster <johnpete.foster@gmail.com>
2025-09-27 08:13:15 -0400

    Worked GCD of strings problem.

---
+++ easy/1071_Greatest_Common_Divisor_of_Strings.js
@@ -0,0 +1,54 @@
+// For two strings s and t, we say "t divides s" if and only if s = t + t + t + ... + t + t (i.e., t is concatenated with itself one or more times).
+
+// Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
+
+ 
+
+// Example 1:
+
+// Input: str1 = "ABCABC", str2 = "ABC"
+// Output: "ABC"
+// Example 2:
+
+// Input: str1 = "ABABAB", str2 = "ABAB"
+// Output: "AB"
+// Example 3:
+
+// Input: str1 = "LEET", str2 = "CODE"
+// Output: ""
+ 
+
+// Constraints:
+
+// 1 <= str1.length, str2.length <= 1000
+// str1 and str2 consist of English uppercase letters.
+
+/**
+ * @param {string} str1
+ * @param {string} str2
+ * @return {string}
+ */
+var gcdOfStrings = function(str1, str2) {
+    if(str1 + str2 != str2 + str1) return ""
+    const gcd = (a, b) => b === 0 ? a : gcd(b, a % b);
+    const gcdLen = gcd(str1.length, str2.length);
+    return str1.slice(0, gcdLen);
+
+
+};
+
+const testCases = [
+    ["ABCABC", "ABC", "ABC"],
+    ["ABABAB", "ABAB","AB"],
+    ["LEET", "CODE", ""],
+    ["TAUXXTAUXXTAUXXTAUXXTAUXX", "TAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXX","TAUXX"]
+                  
+]
+
+let testNumber = 1; 
+for(let[str1, str2, expected] of testCases){
+    const returned = gcdOfStrings(str1, str2); 
+    const result = returned === expected ? `✅ Passed` : `❌ Failed`; 
+    console.log(`Test ${testNumber} ${result} with: ${returned}`); 
+    testNumber ++; 
+}
