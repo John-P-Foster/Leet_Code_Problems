7a85c489c4fec6a50f013df02d12fce2af0465f9
John-P-Foster <johnpete.foster@gmail.com>
2025-10-03 11:32:48 -0400

    Solved Shortest Completing Word

---
+++ easy/748_Shortest_Completing_Word.js
@@ -0,0 +1,88 @@
+// Given a string licensePlate and an array of strings words, find the shortest completing word in words.
+
+// A completing word is a word that contains all the letters in licensePlate. Ignore numbers and spaces 
+// in licensePlate, and treat letters as case insensitive. If a letter appears more than once in licensePlate,
+//  then it must appear in the word the same number of times or more.
+
+// For example, if licensePlate = "aBc 12c", then it contains letters 'a', 'b' (ignoring case), and 
+// 'c' twice. Possible completing words are "abccdef", "caaacab", and "cbca".
+
+// Return the shortest completing word in words. It is guaranteed an answer exists. If there are 
+// multiple shortest completing words, return the first one that occurs in words.
+
+ 
+
+// Example 1:
+
+// Input: licensePlate = "1s3 PSt", words = ["step","steps","stripe","stepple"]
+// Output: "steps"
+// Explanation: licensePlate contains letters 's', 'p', 's' (ignoring case), and 't'.
+// "step" contains 't' and 'p', but only contains 1 's'.
+// "steps" contains 't', 'p', and both 's' characters.
+// "stripe" is missing an 's'.
+// "stepple" is missing an 's'.
+// Since "steps" is the only word containing all the letters, that is the answer.
+// Example 2:
+
+// Input: licensePlate = "1s3 456", words = ["looks","pest","stew","show"]
+// Output: "pest"
+// Explanation: licensePlate only contains the letter 's'. All the words contain 's',
+//  but among these "pest", "stew", and "show" are shortest. The answer is "pest" 
+//  because it is the word that appears earliest of the 3.
+ 
+
+// Constraints:
+
+// 1 <= licensePlate.length <= 7
+// licensePlate contains digits, letters (uppercase or lowercase), or space ' '.
+// 1 <= words.length <= 1000
+// 1 <= words[i].length <= 15
+// words[i] consists of lower case English letters.
+
+/**
+ * @param {string} licensePlate
+ * @param {string[]} words
+ * @return {string}
+ */
+var shortestCompletingWord = function(licensePlate, words) {
+
+    const checkWord = function(letters, orginalWord){
+        if(letters.length > orginalWord.length) return Infinity; 
+        let word = orginalWord
+        for(let char of letters){
+            if(word.includes(char)){
+                word = word.replace(char, "");
+            }else{
+                return Infinity;
+            }
+        }
+
+        return orginalWord.length; 
+    }
+
+    let letters = ""
+    for(char of licensePlate){
+        letters +=  /[a-zA-Z]/.test(char) ? char.toLowerCase() :"";
+    }
+
+    let shortestWord = null; 
+    for(let word of words){
+        shortestWord = (checkWord(letters, word) < (shortestWord?.length ?? Infinity)) ? word : shortestWord
+    }
+
+
+    return shortestWord; 
+};
+
+const testCases = [
+    ["1s3 PSt",["step","steps","stripe","stepple"],"steps"],
+    ["1s3 456",["looks","pest","stew","show"],"pest"]
+]
+
+let testNumber = 1; 
+for(let[licensePlate, words, expected] of testCases ){
+    const returned = shortestCompletingWord(licensePlate, words);
+    const result = returned === expected ? `✅ Passed` : `❌ Failed`;
+    console.log(`Test ${testNumber} ${result} \nExpected: ${expected}\nGot:      ${returned}\n`);
+    testNumber ++;
+}
