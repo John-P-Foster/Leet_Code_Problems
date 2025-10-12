7c5d06de6d201fe7615363ad97386e7f0c9d4290
John-P-Foster <johnpete.foster@gmail.com>
2025-10-12 10:00:25 -0400

    Solved alternating bits problem.

---
+++ easy/693_Binary_Number_with_Alternating_Bits.js
@@ -0,0 +1,55 @@
+// Given a positive integer, check whether it has alternating bits: namely,
+//  if two adjacent bits will always have different values.
+
+ 
+
+// Example 1:
+
+// Input: n = 5
+// Output: true
+// Explanation: The binary representation of 5 is: 101
+// Example 2:
+
+// Input: n = 7
+// Output: false
+// Explanation: The binary representation of 7 is: 111.
+// Example 3:
+
+// Input: n = 11
+// Output: false
+// Explanation: The binary representation of 11 is: 1011.
+ 
+
+// Constraints:
+
+// 1 <= n <= 231 - 1
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var hasAlternatingBits = function(n) {
+
+    while(n != 0){
+        if((n & 1) === ((n >>> 1) & 1))  return false; 
+        n = n >>> 1; 
+    }
+    return true; 
+};
+
+const testCases = [
+    [ 5, true],
+    [ 7, false],
+    [11, false]
+]
+
+
+let testNumber = 1; 
+for(let[n, expected] of testCases){
+    const returned = hasAlternatingBits(n);
+    const result = returned === expected ? `✅ Passed ` : `❌ Failed`;
+    console.log(`Test ${testNumber} ${result}`)
+    console.log(`Expected: ${expected}`);
+    console.log(`Returned: ${returned}\n`);
+    testNumber ++; 
+}
