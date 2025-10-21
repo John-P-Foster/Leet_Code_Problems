9c6f34a7ebde879b9ff98387aa8ebf8aabbf395d
John-P-Foster <johnpete.foster@gmail.com>
2025-10-21 09:47:19 -0400

    Solved Search Insert position

---
+++ easy/35_Search_Insert_Position.js
@@ -0,0 +1,73 @@
+// Given a sorted array of distinct integers and a target value, return the index if the target is found. 
+// If not, return the index where it would be if it were inserted in order.
+
+// You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,3,5,6], target = 5
+// Output: 2
+// Example 2:
+
+// Input: nums = [1,3,5,6], target = 2
+// Output: 1
+// Example 3:
+
+// Input: nums = [1,3,5,6], target = 7
+// Output: 4
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 104
+// -104 <= nums[i] <= 104
+// nums contains distinct values sorted in ascending order.
+// -104 <= target <= 104
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    let left = 0; 
+    let right = nums.length - 1; 
+
+    while(left <= right){
+        const mid = Math.floor((right - left) / 2) + left; 
+        if(nums[mid] === target) return mid; 
+        if(nums[mid] < target ){
+            left = mid + 1; 
+        }else{
+            right = mid - 1 ; 
+        }
+
+      
+    }
+    
+    return left; 
+};
+
+
+const testCases = [
+    [[1, 3, 5, 6], 5, 2],
+    [[1, 3, 5, 6], 2, 1],
+    [[1, 3, 5, 6], 7, 4],
+    [[1, 3, 5, 7, 9], 5, 2],
+    [[1, 3, 5, 7, 9], 6, 3],
+    [[1, 3, 5, 7, 9], 2, 1],
+
+]
+
+
+let testNumber = 1; 
+for(let[nums, target, expected] of testCases){
+    const returned = searchInsert(nums, target);
+    const result = returned.toString() === expected.toString() ? `✅ Passed ` : `❌ Failed`;
+    console.log(`Test ${testNumber} ${result}`)
+    console.log(`Expected: ${expected}`);
+    console.log(`Returned: ${returned}\n`);
+    testNumber ++; 
+}
