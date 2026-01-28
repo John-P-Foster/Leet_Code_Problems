ffe551452343bd3c34626c05f187a3fee7d57723
John-P-Foster <johnpete.foster@gmail.com>
2026-01-28 09:58:41 -0500

    Solved Day of the week problem.

--- random_leet_code_problem.js
+++ random_leet_code_problem.js
@@ -1,5 +1,15 @@
-function randomProblemNumber(){
-    return 'Try problem closest to the number: ' + (Math.floor( Math.random() * 3000) + 1);
+function randomProblemNumber(min, max){
+    let random = Math.random();
+    console.log(`
+        Random: ${random}
+        Range: ${max - min + 1}
+        Start: ${min}
+        End: ${max}
+        value: ${(random * (max - min  + 1)) + min}
+    `)
+
+    const ranomInt = (min, max) => Math.floor(random * (max - min + 1)) + min
+    return `Try problem closest to the number: ${ranomInt(min, max)} `
 }
 
-console.log(randomProblemNumber()); 
+console.log(randomProblemNumber(1,3000)); 
