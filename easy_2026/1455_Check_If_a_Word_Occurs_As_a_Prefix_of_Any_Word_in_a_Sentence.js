e5c88ee2eaf066249093df04cbc4ed15a797260a
John-P-Foster <johnpete.foster@gmail.com>
2026-02-23 09:47:22 -0500

    Solved check if a word is a prefix to a word in a sentence.

---
+++ easy_2026/1455_Check_If_a_Word_Occurs_As_a_Prefix_of_Any_Word_in_a_Sentence.js
@@ -0,0 +1,77 @@
+// Given a sentence that consists of some words separated by a single space, and a searchWord, check if 
+// searchWord is a prefix of any word in sentence.
+
+// Return the index of the word in sentence (1-indexed) where searchWord is a prefix of this word. If 
+// searchWord is a prefix of more than one word, return the index of the first word (minimum index). If there is no such word return -1.
+
+// A prefix of a string s is any leading contiguous substring of s.
+
+ 
+
+// Example 1:
+
+// Input: sentence = "i love eating burger", searchWord = "burg"
+// Output: 4
+// Explanation: "burg" is prefix of "burger" which is the 4th word in the sentence.
+// Example 2:
+
+// Input: sentence = "this problem is an easy problem", searchWord = "pro"
+// Output: 2
+// Explanation: "pro" is prefix of "problem" which is the 2nd and the 6th word in the sentence, but we 
+// return 2 as it's the minimal index.
+// Example 3:
+
+// Input: sentence = "i am tired", searchWord = "you"
+// Output: -1
+// Explanation: "you" is not a prefix of any word in the sentence.
+ 
+
+// Constraints:
+
+// 1 <= sentence.length <= 100
+// 1 <= searchWord.length <= 10
+// sentence consists of lowercase English letters and spaces.
+// searchWord consists of lowercase English letters.
+
+/**
+ * @param {string} sentence
+ * @param {string} searchWord
+ * @return {number}
+ */
+var isPrefixOfWord = function(sentence, searchWord) {
+    let words = sentence.split(' '); 
+
+    for(let i = 0; i < words.length; i++){
+        let word = words[i]; 
+        if(word.length < searchWord.length) continue; 
+        
+        let prefix = true; 
+        for(let j = 0; j < searchWord.length; j ++){
+            if(word[j] !== searchWord[j]){
+                prefix = false; 
+                break; 
+            }
+        }
+
+        if(prefix) return i + 1; 
+    }
+
+    return -1; 
+};
+
+
+const testCases = [
+    ["i love eating burger", "burg", 4],
+    ["this problem is an easy problem", "pro", 2],
+    ["i am tired", "you", -1],
+]
+
+testCases.forEach((test, testNumber) => {
+const expected = test[test.length - 1]; 
+const params = test.slice(0, -1); 
+const returned = isPrefixOfWord(...params);
+const result = returned.toString() === expected.toString() ? `✅ Passed ` : `❌ Failed`;
+console.log(`Test ${testNumber + 1} ${result}`)
+console.log(`Expected: ${expected}`);
+console.log(`Returned: ${returned}\n`);
+})
