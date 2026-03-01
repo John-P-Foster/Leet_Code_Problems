b1ff558c75b21189aa4cccbac02f8d3ca15c2b31
John-P-Foster <johnpete.foster@gmail.com>
2026-02-28 21:09:46 -0500

    Solved Subtract the sum form product of digits of an integer.

---
+++ easy_2026/1281_Subtract_the_Product_and_Sum_of_Digits_of_an_Integer.js
@@ -0,0 +1,62 @@
+// 1281. Subtract the Product and Sum of Digits of an Integer
+// Easy
+// Topics
+// premium lock icon
+// Companies
+// Hint
+// Given an integer number n, return the difference between the product of its digits and the sum of its digits.
+ 
+
+// Example 1:
+
+// Input: n = 234
+// Output: 15 
+// Explanation: 
+// Product of digits = 2 * 3 * 4 = 24 
+// Sum of digits = 2 + 3 + 4 = 9 
+// Result = 24 - 9 = 15
+// Example 2:
+
+// Input: n = 4421
+// Output: 21
+// Explanation: 
+// Product of digits = 4 * 4 * 2 * 1 = 32 
+// Sum of digits = 4 + 4 + 2 + 1 = 11 
+// Result = 32 - 11 = 21
+ 
+
+// Constraints:
+
+// 1 <= n <= 10^5
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var subtractProductAndSum = function(n) {
+    let product = 1; 
+    let sum = 0; 
+    while(n > 0){
+        let digit = n % 10; 
+        product *= digit; 
+        sum += digit; 
+        n = Math.floor(n / 10); 
+    }
+
+    return product - sum; 
+};
+
+const testCases = [
+    [234, 15],
+    [4421, 21],
+]
+
+testCases.forEach((test, testNumber) => {
+const expected = test[test.length - 1]; 
+const params = test.slice(0, -1); 
+const returned = subtractProductAndSum(...params);
+const result = returned.toString() === expected.toString() ? `✅ Passed ` : `❌ Failed`;
+console.log(`Test ${testNumber + 1} ${result}`)
+console.log(`Expected: ${expected}`);
+console.log(`Returned: ${returned}\n`);
+})
